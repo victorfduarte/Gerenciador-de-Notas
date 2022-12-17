103110a85f6d4b6e7acaa702f97c08b09c690695
Paulo Victor <victorfduarte18@gmail.com>
2022-12-17 17:46:00 -0300

    adição de novas tabelas

--- dados/tabelas.py
+++ dados/tabelas.py
@@ -1,34 +1,39 @@
 from . import base_table as bt
 from . import fields
-from . import setter as st
 
 
-class Avaliacao(bt.Table):
-    num_av = fields.Field()
-    nota = fields.Field()
-    data = fields.Field()    
+class Professor(bt.Table):
+    nome = fields.Field()
+    sobrenome = fields.Field()
+    sexo = fields.Field()
 
-    def __repr__(self):
-        return f'Avaliacao(num_av = {self.num_av})'
+
+class Telefone(bt.Table):
+    cod_prof = fields.ForeignKey(Professor)
+    telefone = fields.Field()
+
+
+class Email(bt.Table):
+    cod_prof = fields.ForeignKey(Professor)
+    email = fields.Field()
 
 
 class Materia(bt.Table):
     nome = fields.Field()
-    prof = fields.Field()
-    dias = fields.Field()
-    avaliacoes = fields.ForeignKey(Avaliacao)
+    prof = fields.ForeignKey(Professor)
+    obs = fields.Field()
     
     def __repr__(self):
-        return f'Materia(nome = {self.nome}, avaliacoes = {self.avaliacoes})'
+        return f'Materia(nome = {self.nome}, professor = {self.prof})'
 
 
-if __name__ == '__main__':
+class Avaliacao(bt.Table):
+    nota = fields.Field()
+    materia = fields.ForeignKey(Materia)
+    data = fields.Field()
+    tipo = fields.Field()
+    peso = fields.Field()
 
-    avaliacao_data = st.load('Avaliacao')['values']
-
-    a1 = Avaliacao(**avaliacao_data[0])
-    a2 = Avaliacao(**avaliacao_data[1])
-
-    Avaliacao.show_regs()
-
+    def __repr__(self):
+        return f'Avaliacao(nota = {self.nota})'
 
